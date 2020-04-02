53c26ec8b0fbca96536b5feb86deeea6a2e62e4b
Gábor Csárdi <csardi.gabor@gmail.com>
2020-04-02 02:20:34 +0100

    Make app_process$stop() compatible with covr
    It should not kill the subprocess, because then
    it cannot write out the coverage results.

--- R/app-process.R
+++ R/app-process.R
@@ -36,7 +36,23 @@ new_app_process <- function(app) {
     get_port = function() self$.port,
 
     stop = function() {
-      self$.process$kill()
+      # The details are important here, for the sake of covr,
+      # so that we can test the pressr package itself.
+      # 1. The subprocess serving the app is in Sys.sleep(), which we
+      #    need to interrupt first.
+      # 2. Then we need to read out the result of that $call()
+      #    (i.e. the interruption), because otherwise the subprocess is
+      #    stuck at a blocking write() system call, and cannot be
+      #    interrupted in the $close() call, and will be killed, and
+      #    then it cannot write out the coverage results.
+      # 3. Once we $read(), we can call $close() because that will
+      #    close the standard input of the subprocess, which is reading
+      #    the standard input, so it will quit.
+
+      self$.process$interrupt()
+      self$.process$poll_process(1000)
+      self$.process$read()
+      self$.process$close()
       self$.process <- NULL
       invisible(self)
     },
