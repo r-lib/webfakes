1124f3e0de486c7b42b5c76c8e77dc76c17dedac
Gábor Csárdi <csardi.gabor@gmail.com>
2020-04-08 18:26:38 +0100

    Add pkgdown web site

--- R/package.R
+++ R/package.R
@@ -1,9 +1,3 @@
 
-#' Lightweight Web Server for Testing
-#'
-#' Lightweight Web Server for Testing
-#'
 #' @useDynLib presser, .registration = TRUE, .fixes = "c_"
-#' @docType package
-#' @name presser
 NULL
