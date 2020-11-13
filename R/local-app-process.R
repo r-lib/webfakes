12a4643d2f6e9af467a64d39f1c7b04dacd69ae4
Gábor Csárdi <csardi.gabor@gmail.com>
2020-11-13 09:44:36 +0000

    local_app_process() does not need .env
    Just eval normally.

--- R/local-app-process.R
+++ R/local-app-process.R
@@ -7,17 +7,14 @@
 #'
 #' @inheritParams new_app_process
 #' @param ... Passed to [new_app_process()].
-#' @param .env The environment to run [new_app_process()] in.
 #' @param .local_envir The environment to attach the process cleanup to.
 #'   Typically a frame. When this frame finishes, the process is stopped.
 #'
 #' @export
 #' @seealso [new_app_process()] for more details.
 
-local_app_process <- function(app, ..., .env = parent.frame(),
-                              .local_envir = parent.frame()) {
-
-  proc <- do.call("new_app_process", list(app, ...), envir = .env)
+local_app_process <- function(app, ..., .local_envir = parent.frame()) {
+  proc <- new_app_process(app, ...)
   withr::defer(proc$stop(), envir = .local_envir)
   proc
 }
