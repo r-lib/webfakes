ef8a7a192116e281a5d5ed95a788d8b2795edd6d
Gábor Csárdi <csardi.gabor@gmail.com>
2025-01-12 09:00:57 +0100

    Fix Mbed TLS compilation on Windows

--- src/mbedtls/library/entropy_poll.c
+++ src/mbedtls/library/entropy_poll.c
@@ -42,6 +42,31 @@
 #include <bcrypt.h>
 #include <intsafe.h>
 
+#if defined(__MINGW32__)
+// MINGW
+int mbedtls_platform_entropy_poll(void *data, unsigned char *output, size_t len,
+                           size_t *olen)
+{
+    HCRYPTPROV provider;
+    ((void) data);
+    *olen = 0;
+
+    if (CryptAcquireContext(&provider, NULL, NULL,
+                            PROV_RSA_FULL, CRYPT_VERIFYCONTEXT ) == FALSE ) {
+        return( MBEDTLS_ERR_ENTROPY_SOURCE_FAILED );
+    }
+
+    if (CryptGenRandom(provider, (DWORD) len, output) == FALSE ) {
+        CryptReleaseContext( provider, 0 );
+        return(MBEDTLS_ERR_ENTROPY_SOURCE_FAILED);
+    }
+
+    CryptReleaseContext(provider, 0);
+    *olen = len;
+
+    return 0;
+}
+#else
 int mbedtls_platform_entropy_poll(void *data, unsigned char *output, size_t len,
                                   size_t *olen)
 {
@@ -68,6 +93,7 @@ int mbedtls_platform_entropy_poll(void *data, unsigned char *output, size_t len,
 
     return 0;
 }
+#endif
 #else /* _WIN32 && !EFIX64 && !EFI32 */
 
 /*
