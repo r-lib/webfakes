40e0e7f4a7e10255863949cce2dc2659c1786901
Gábor Csárdi <csardi.gabor@gmail.com>
2026-05-15 11:20:13 +0200

    Avoid |>, webfakes works with older R as well

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,12 +1,8 @@
 test_that("http_time_stamp ignores local locale", {
   withr::local_locale(LC_TIME = "es_ES.UTF-8")
 
-  Sys.getlocale("LC_TIME") |>
-    expect_equal("es_ES.UTF-8")
+  expect_equal(Sys.getlocale("LC_TIME"), "es_ES.UTF-8")
 
   # If httr::parse_http_date works it means time is not in local locale
-  http_time_stamp() |>
-    httr::parse_http_date() |>
-    is.na() |>
-    expect_false()
+  expect_false(is.na(httr::parse_http_date(http_time_stamp())))
 })
